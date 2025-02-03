f36a451ccfb3fa5434002d17d3b54c6a7f5c9a4e
Methdec <polowador@gmail.com>
2025-02-03 13:11:02 +0100

    aztkl

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -1,8 +1,8 @@
 import { useAuth } from "../../auth/AuthProvider";
 import { Login, Logout } from "../../auth/LoginOut";
 import React from "react";
-import { Link } from "react-router-dom";
-import "./Header.scss";
+import { Link } from "react-router";
+import "./header.scss";
 
 export default function Header() {
   const { user } = useAuth();
